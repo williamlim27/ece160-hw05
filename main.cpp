2d6d6d86c9b37bd004f031115e1527131be79ffa
Julia Astrauckas <astrocats@spotify.com>
2017-11-15 17:38:22 -0500

    more files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "Student.h"
+
+int main() {
+  Student s("Peter", "Cooper", 1859, Major::EE);
+  for (float g = 3.5f; g <= 4.0f; g += 0.1f) {
+    s.addGrade(g);
+  }
+  s.printInfo();
+  s.addGrade(4.0);
+  s.printInfo();
+  return 0;
+}
